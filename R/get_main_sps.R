dedec3a649bec354f5d9b854d6daa1058eca6155
Aitor Ameztegui <ameztegui@gmail.com>
2024-05-28 10:43:18 +0200

    comentar lineas

--- R/get_main_sps.R
+++ R/get_main_sps.R
@@ -4,5 +4,5 @@ get_main_sps <- function(df, n_species, criterion, threshold) {
 
 }
 
-deberia dar las n especies mas importantes, segun el criterio definido, de mas a menos.
-Si se define threshold, solo las que lo superen (pensar que hago con el resto!)
+# deberia dar las n especies mas importantes, segun el criterio definido, de mas a menos.
+# Si se define threshold, solo las que lo superen (pensar que hago con el resto!)
