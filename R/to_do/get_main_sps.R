71600b594e2aea1401b85417fd551bf7f91833da
ameztegui <ameztegui@gmail.com>
2024-05-31 00:11:52 +0200

    Initial commit

---
+++ R/to_do/get_main_sps.R
@@ -0,0 +1,8 @@
+
+get_main_sps <- function(df, n_species, criterion, threshold) {
+
+
+}
+
+# deberia dar las n especies mas importantes, segun el criterio definido, de mas a menos.
+# Si se define threshold, solo las que lo superen (pensar que hago con el resto!)
